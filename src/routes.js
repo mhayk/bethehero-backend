87d4477ccdbdcfe11ced55508c4810ead29e69d4
Mhayk Whandson <hi@mhayk.com>
2020-03-24 16:20:14 +0000

    The endpoint ongs via POST to create ongs is ready.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-  const { body } = request;
+routes.post('/ongs', async (request, response) => {
+  const {
+    name, email, whatsapp, city, uf,
+  } = request.body;
 
-  console.log(body);
+  const id = crypto.randomBytes(4).toString('HEX');
 
-  return response.json({
-    evento: 'Be the Hero',
-    name: 'Mhayk Whandson',
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
   });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
