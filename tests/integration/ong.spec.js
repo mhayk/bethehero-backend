826c0b03ddd396c24f44ab230e09f40bac8c9743
Mhayk Whandson <hi@mhayk.com>
2020-04-03 19:46:30 +0100

    supertest has been installed and a test was created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -1,5 +1,27 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
 describe('ONG', () => {
-    it('should be able to create a new ONG', () => {
-        
-    })
-})
+  beforeEach(async () => {
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs').send({
+        name: 'APAD',
+        email: 'hi@mhayk.com',
+        whatsapp: '4407465783231',
+        city: 'London',
+        uf: 'UK',
+      });
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  });
+});
